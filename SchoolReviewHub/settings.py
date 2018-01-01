d547186efc54fd938af331414fc7af4c79e0b7d1
Nicholas Kajoh <kajohterna@gmail.com>
2018-01-01 18:11:21 +0100

    fix: add static root in settings

--- SchoolReviewHub/settings.py
+++ SchoolReviewHub/settings.py
@@ -135,4 +135,6 @@ STATICFILES_DIRS = [
   os.path.join(BASE_DIR, 'build/static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
