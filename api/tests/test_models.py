d86f2bbd0439fa3c0271944292c3ddad675ea2ac
Nicholas Kajoh <kajohterna@gmail.com>
2018-01-03 08:30:11 +0100

    test: prep django test suite

---
+++ api/tests/test_models.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from api.models import School, Review, Comment, Comparison, Criterion, Report, Upvote
+
+class SchoolTestCase(TestCase):
+  def setUp(self):
+    pass
+
+
+class ReviewTestCase(TestCase):
+  def setUp(self):
+    pass
+
+
+class CommentTestCase(TestCase):
+  def setUp(self):
+    pass
+
+
+class ComparisonTestCase(TestCase):
+  def setUp(self):
+    pass
+
+
+class CriterionTestCase(TestCase):
+  def setUp(self):
+    pass
+
+
+class ReportTestCase(TestCase):
+  def setUp(self):
+    pass
+
+
+class UpvoteTestCase(TestCase):
+  def setUp(self):
+    pass
