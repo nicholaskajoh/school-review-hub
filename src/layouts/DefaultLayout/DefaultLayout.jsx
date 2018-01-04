3d4e18e854e9815c9526f9e72855050b61597b6e
Nicholas Kajoh <kajohterna@gmail.com>
2018-01-04 12:06:30 +0100

    feat: add default site layout

---
+++ src/layouts/DefaultLayout/DefaultLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import './DefaultLayout.css';
+import Header from '../../partials/Header/Header';
+import Footer from '../../partials/Footer/Footer';
+
+const DefaultLayout = ({component: Component, ...rest}) => (
+  <Route {...rest} render={matchProps => (
+    <div>
+      <Header />
+      <Component {...matchProps} />
+      <Footer />
+    </div>
+  )} />
+);
+
+export default DefaultLayout;
