31f9303b738c1912dbb89bb05d96fd6d679515b0
Nicholas Kajoh <kajohterna@gmail.com>
2018-01-07 00:14:17 +0100

    refactor: rename ReviewForm component to RatingForm

---
+++ src/pages/RatingForm/RatingForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RatingForm.css';
+
+const RatingForm = () => (
+  <div className="section">
+    <div className="container">
+      <h1 className="title">RatingForm page</h1>
+    </div>
+  </div>
+);
+
+export default RatingForm;
