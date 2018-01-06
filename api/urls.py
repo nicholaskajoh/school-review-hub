9b6798feabe92f56a1ad9e84f535b7a4fbd48720
Nicholas Kajoh <kajohterna@gmail.com>
2018-01-07 00:10:33 +0100

    fix: route all urls except /admin and /api to React app to avoid Django 404s

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 from .views import *
 
 urlpatterns = [
-	path('token-auth/', obtain_auth_token),
+	path('token-auth', obtain_auth_token),
 	path('school/<int:school_id>', SchoolView.as_view(), name='school'),
 ]
