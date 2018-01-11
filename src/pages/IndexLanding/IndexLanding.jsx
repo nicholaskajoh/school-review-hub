2e80b9b667173c05df01959178b179052f26f89d
Nicholas Kajoh <kajohterna@gmail.com>
2018-01-11 13:36:28 +0100

    feat: design index landing page

--- src/pages/IndexLanding/IndexLanding.jsx
+++ src/pages/IndexLanding/IndexLanding.jsx
@@ -1,11 +1,73 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './IndexLanding.css';
 
 const IndexLanding = () => (
-  <div className="section">
-    <div className="container">
-      <h1 className="title">IndexLanding page</h1>
-    </div>
+  <div>
+    <section className="hero is-medium is-dark is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">
+            This is SchoolReviewHub
+          </h1>
+          <h2 className="subtitle">
+            Crowd-sourced anonymous reviews of Nigerian univeristies at your finger tips!
+          </h2>
+        </div>
+      </div>
+    </section>
+
+    <section className="section">
+      <div className="container">
+        <nav className="columns">
+          <div className="column has-text-centered">
+            <div>
+              <p><i className="fa fa-star fa-2x"></i></p>
+              <p className="title">Rate</p>
+              <p>Rate univeristies using our comparison-based system. It allows you match two univeristies against each other and choose the best under different criteria.</p>
+            </div>
+          </div>
+          <div className="column has-text-centered">
+            <div>
+              <p><i className="fa fa-comments fa-2x"></i></p>
+              <p className="title">Review</p>
+              <p>Write reviews of univeristies detailing more nuanced opinions that our rating system might not capture.</p>
+            </div>
+          </div>
+          <div className="column has-text-centered">
+            <div>
+              <p><i className="fa fa-microphone fa-2x"></i></p>
+              <p className="title">Report</p>
+              <p>Report events -- good, bad and ugly -- happening in any univeristy that might be of interest to the public.</p>
+            </div>
+          </div>
+          <div className="column has-text-centered">
+            <div>
+              <p><i className="fa fa-trophy fa-2x"></i></p>
+              <p className="title">The Index</p>
+              <p>SchoolReviewHub maintains an index of all univeristies in Nigeria listed in ascending order of their rankings. Easily find out the best univeristies as rated by you!</p>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </section>
+
+    <section className="hero is-warning">
+      <div className="hero-body has-text-centered">
+        <div className="container">
+          <h1 className="title">
+            You're anonymous!
+          </h1>
+          <h2 className="subtitle">
+            Rate, review and report anonymously. Tell the truth and nothing but the truth.
+          </h2>
+          <p>What are you waiting for? Sign up for or Login to SchoolReviewHub...</p>
+
+          <Link className="button is-success is-medium" to="/sign-up">Sign Up</Link> &nbsp;
+          <Link className="button is-info is-medium" to="/login">Login</Link>
+        </div>
+      </div>
+    </section>
   </div>
 );
 
--- src/partials/Footer/Footer.jsx
+++ src/partials/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => (
   <footer className="footer">
     <div className="container">
       <div className="content has-text-centered">
-        <p>&copy; SchoolReviewHub, {new Date().getFullYear()}</p>
+        <p>&copy; SchoolReviewHub, {new Date().getFullYear()}. Built with <i className="fa fa-heart has-text-danger"></i> by <a href="https://twitter.com/nicholaskajoh">Nick</a>, <a href="https://twitter.com/TNkemdilim">Toks</a>, <a href="https://twitter.com/johnayeni_">John</a> and <a href="https://twitter.com/olaoluwa_98">Awo</a>.</p>
       </div>
     </div>
   </footer>
