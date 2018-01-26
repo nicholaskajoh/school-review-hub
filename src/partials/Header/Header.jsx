89eee008a139ad6e7fac991ce47be6f0f3afff98
Nicholas Kajoh <kajohterna@gmail.com>
2018-01-26 08:37:45 +0100

    feat: add/remove nav links based on auth status

--- src/partials/Header/Header.jsx
+++ src/partials/Header/Header.jsx
@@ -4,67 +4,75 @@ import './Header.css';
 import logo from '../../logo.png';
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {isNavbarActive: false};
-  }
+    constructor(props) {
+        super(props);
+        this.state = {isNavbarActive: false};
+    }
 
-  toggleNavbar = () => {
-    this.setState(prevState => ({
-      isNavbarActive: !prevState.isNavbarActive
-    }));
-  }
+    toggleNavbar = () => {
+        this.setState(prevState => ({
+            isNavbarActive: !prevState.isNavbarActive
+        }));
+    }
+
+    isAuth() {
+        return localStorage.getItem("authToken") !== null;
+    }
 
   render() {
     return (
-      <nav className="navbar is-warning is-fixed-top">
-        <div className="navbar-brand">
-          <Link className="navbar-item" to="/">
-            <img src={logo} alt="SchoolReviewHub" width="40px" height="auto"/>
-          </Link>
-          <div className={"navbar-burger burger" + (this.state.isNavbarActive ? " is-active" : "")} onClick={this.toggleNavbar}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </div>
-
-        <div className={"navbar-menu" + (this.state.isNavbarActive ? " is-active" : "")} onClick={this.toggleNavbar}>
-          <div className="navbar-start">
-
-          </div>
-
-          <div className="navbar-end">
-            <Link className="navbar-item" to="/home">Home</Link>
-            <Link className="navbar-item" to="/srh-index">SRH Index</Link>
-            <Link className="navbar-item" to="/match">Match & Rate</Link>
-            <Link className="navbar-item" to="/search">
-              <i className="fa fa-search"></i>
-            </Link>
-
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">Account</a>
-              
-              <div className="navbar-dropdown is-boxed">
-                <Link className="navbar-item" to="/profile">Profile</Link>
-                <Link className="navbar-item" to="/help">Help</Link>
-                <a className="navbar-item">Log out</a>
-              </div>
+        <nav className="navbar is-warning is-fixed-top">
+            <div className="navbar-brand">
+                <Link className="navbar-item" to="/">
+                    <img src={logo} alt="SchoolReviewHub" width="40px" height="auto"/>
+                </Link>
+                <div className={"navbar-burger burger" + (this.state.isNavbarActive ? " is-active" : "")} onClick={this.toggleNavbar}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
             </div>
 
-            <div className="navbar-item">
-              <div className="field is-grouped">
-                <p className="control">
-                  <Link className="button is-success" to="/register">Sign Up</Link>
-                </p>
-                <p>
-                  <Link className="button is-info" to="/login">Login</Link>
-                </p>
-              </div>
+            <div className={"navbar-menu" + (this.state.isNavbarActive ? " is-active" : "")} onClick={this.toggleNavbar}>
+                <div className="navbar-start">
+
+                </div>
+
+                <div className="navbar-end">
+                    {this.isAuth() ?<Link className="navbar-item" to="/home">Home</Link>: ""}
+                    <Link className="navbar-item" to="/srh-index">SRH Index</Link>
+                    {this.isAuth() ? <Link className="navbar-item" to="/match">Match & Rate</Link>: ""}
+                    <Link className="navbar-item" to="/search">
+                    <i className="fa fa-search"></i>
+                    </Link>
+
+                    {this.isAuth() ?
+                    <div className="navbar-item has-dropdown is-hoverable">
+                        <a className="navbar-link">Account</a>
+                        
+                        <div className="navbar-dropdown is-boxed">
+                            <Link className="navbar-item" to="/profile">Profile</Link>
+                            <Link className="navbar-item" to="/help">Help</Link>
+                            <Link className="navbar-item" to="/logout">Log out</Link>
+                        </div>
+                    </div>
+                    : ""}
+
+                    {!this.isAuth() ?
+                    <div className="navbar-item">
+                        <div className="field is-grouped">
+                            <p className="control">
+                            <Link className="button is-success" to="/register">Sign Up</Link>
+                            </p>
+                            <p>
+                            <Link className="button is-info" to="/login">Login</Link>
+                            </p>
+                        </div>
+                    </div>
+                    : ""}
+                </div>
             </div>
-          </div>
-        </div>
-      </nav>
+        </nav>
     );
   }
 }
