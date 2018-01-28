8c5fbf39427ad184db055e3ba16e4f6a5a180448
Nicholas Kajoh <kajohterna@gmail.com>
2018-01-28 21:38:31 +0100

    style: format to 2 spaces

--- src/auth.js
+++ src/auth.js
@@ -4,16 +4,16 @@ import DefaultLayout from './layouts/DefaultLayout/DefaultLayout';
 
 // Auth middleware
 const PrivateRoute = ({ component: Component, ...rest }) => (
-    <Route {...rest} render={props => (
-        localStorage.getItem("authToken") !== null ? (
-        <DefaultLayout {...rest} component={Component}/>
-        ) : (
-        <Redirect to={{
-            pathname: '/login',
-            state: { from: props.location }
-        }}/>
-        )
-    )}/>
+  <Route {...rest} render={props => (
+    localStorage.getItem("authToken") !== null ? (
+    <DefaultLayout {...rest} component={Component}/>
+    ) : (
+    <Redirect to={{
+      pathname: '/login',
+      state: { from: props.location }
+    }}/>
+    )
+  )}/>
 )
 
 export default PrivateRoute;
