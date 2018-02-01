97f5ba3476ee07b43f246672426031c8b1568fbc
Emmanuel Awotunde <awotunde.emmanuel1@gmail.com>
2018-02-01 16:50:50 +0100

    test: #12 ensure SchoolReportsView is working properly

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -26,15 +26,15 @@ class SchoolReviewsViewTestCase(TestCase):
       name='CU',location='Ogun',logo_url='',website='covenantuniversity.edu.ng',
       rank=1,rating=140.0,
     )
-    self.review2 = ReviewFactory(reviewer=self.user1, school=self.school)
-    self.review1 = ReviewFactory(reviewer=self.user2, school=self.school)
+    self.review1 = ReviewFactory(reviewer=self.user1, school=self.school)
+    self.review2 = ReviewFactory(reviewer=self.user2, school=self.school)
 
   def test_school_reviews_endpoint_is_working(self):
     url = reverse('school_reviews', kwargs={'id':self.school.pk,'page':1})
     response = self.client.get(url)
     self.assertEqual(response.status_code, 200)
 
-  def test_if_endpoint_returns_review(self):    
+  def test_if_endpoint_returns_review(self):
     url = reverse('school_reviews', kwargs={'id':self.school.pk,'page':1})
     response = self.client.get(url)
     reviews = Review.objects.filter(school=self.school).order_by('-updated_at')
@@ -50,4 +50,40 @@ class SchoolReviewsViewTestCase(TestCase):
     serialized = ReviewSerializer(reviews, many=True)
     self.assertEqual(response.data, serialized.data)
 
-  # self.assertContains(response, '1-title')    
+class SchoolReportsViewTestCase(TestCase):
+
+  def setUp(self):
+    self.user1 = UserFactory(email='user1@srh.com')
+    self.user2 = UserFactory(email='user2@srh.com')
+    self.school = SchoolFactory(
+      name='CU',location='Ogun',logo_url='',website='covenantuniversity.edu.ng',
+      rank=1,rating=140.0,
+    )
+    self.report = ReportFactory(reporter=self.user1, school=self.school)
+    self.report2 = ReportFactory(reporter=self.user2, school=self.school)
+
+  def test_school_reports_endpoint_is_working(self):
+    url = reverse('school_reports', kwargs={'id':self.school.pk,'page':1})
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 200)
+
+  def test_if_endpoint_returns_report(self):
+    url = reverse('school_reports', kwargs={'id':self.school.pk,'page':1})
+    response = self.client.get(url)
+    reports = Report.objects.filter(school=self.school).order_by('-updated_at')
+    reports = paginate(reports, 1, 10)
+    serialized = ReportSerializer(reports, many=True)
+    self.assertEqual(response.data, serialized.data)
+
+  def test_if_endpoint_returns_first_page_result_on_empty_page(self):
+    # set page to a page higher than number of pages
+    url = reverse('school_reports', kwargs={'id':self.school.pk,'page':10})
+    response = self.client.get(url)
+    reports = Report.objects.filter(school=self.school).order_by('-updated_at')
+    # get result of the first page
+    reports = paginate(reports, 1, 10)
+    serialized = ReportSerializer(reports, many=True)
+    # it should return the first page results
+    self.assertEqual(response.data, serialized.data)
+
+  # self.assertContains(response, '1-title')
