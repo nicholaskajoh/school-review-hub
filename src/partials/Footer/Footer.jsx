bb299e69f979359951ef35f021d585da8a3cd490
Nicholas Kajoh <kajohterna@gmail.com>
2018-02-28 04:51:36 +0100

    fix: footer tweet button text

--- src/partials/Footer/Footer.jsx
+++ src/partials/Footer/Footer.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Footer.css';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
 
 const Footer = () => (
   <footer className="footer is-dark">
     <div className="container">
       <div className="content has-text-centered">
         <p>
-          &copy; SchoolReviewHub, {new Date().getFullYear()}. Built with{' '}
-          <i className="fa fa-heart has-text-danger" /> by{' '}
-          <a href="https://twitter.com/nicholaskajoh">Nick</a>,{' '}
-          <a href="https://twitter.com/TNkemdilim">Toks</a>,{' '}
-          <a href="https://twitter.com/johnayeni_">John</a> and{' '}
+          &copy; SchoolReviewHub, {new Date().getFullYear()}. Built with{" "}
+          <i className="fa fa-heart has-text-danger" /> by{" "}
+          <a href="https://twitter.com/nicholaskajoh">Nick</a>,{" "}
+          <a href="https://twitter.com/TNkemdilim">Toks</a>,{" "}
+          <a href="https://twitter.com/johnayeni_">John</a> and{" "}
           <a href="https://twitter.com/olaoluwa_98">Awo</a>.
         </p>
         <br />
@@ -22,7 +21,7 @@ const Footer = () => (
           to="https://twitter.com/intent/tweet?text=Check%20out%20School%20Review%20Hub&url=https://schoolreviewhub.herokuapp.com"
           data-size="large"
         >
-          <i className="fab fa-twitter" /> Tweet at us
+          <i className="fab fa-twitter" /> Tweet about us
         </Link>
       </div>
     </div>
