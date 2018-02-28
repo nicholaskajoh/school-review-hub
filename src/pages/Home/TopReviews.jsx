c06c74930ef37c842ef5f38363789824449fe97c
John Ayeni <john.ayeni@stu.cu.edu.ng>
2018-02-28 19:51:04 +0100

    chore: restyle reload button on all pages

--- src/pages/Home/TopReviews.jsx
+++ src/pages/Home/TopReviews.jsx
@@ -1,50 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import TimeAgo from 'react-time-ago';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import TimeAgo from "react-time-ago";
 
 const TopReviews = ({ reviews, isLoaded, errorLoading, spinner, reload }) => {
   let rendering;
-  if (isLoaded)
-  {
-    rendering = (
-      reviews.map((review, index) => (
-        <div className="card" style={{ marginBottom: 15 }}>
-          <header className="card-header">
-            <p className="card-header-title">{review.school.name}</p>
-          </header>
-          <div className="card-content">
-            <div className="content">
-              {review.content.substring(0, 150).trim() +
-                (review.content.length > 150 ? '...' : '')}{' '}
-                <br />
-                <strong><TimeAgo>{new Date(review.created_at)}</TimeAgo></strong>
-            </div>
+  if (isLoaded) {
+    rendering = reviews.map((review, index) => (
+      <div className="card" style={{ marginBottom: 15 }}>
+        <header className="card-header">
+          <p className="card-header-title">{review.school.name}</p>
+        </header>
+        <div className="card-content">
+          <div className="content">
+            {review.content.substring(0, 150).trim() +
+              (review.content.length > 150 ? "..." : "")}{" "}
+            <br />
+            <strong>
+              <TimeAgo>{new Date(review.created_at)}</TimeAgo>
+            </strong>
           </div>
-          <footer className="card-footer">
-            <div className="card-footer-item">
-              <Link to={"/review/" + review.id}>Full review</Link>
-            </div>
-            <div className="card-footer-item">
-              Upvotes ({review.upvotes})
-            </div>
-            <div className="card-footer-item">
-              Comments ({review.comments_count})
-            </div>
-          </footer>
         </div>
-      ))
-    )
-  }
-  else
-  {
+        <footer className="card-footer">
+          <div className="card-footer-item">
+            <Link to={"/review/" + review.id}>Full review</Link>
+          </div>
+          <div className="card-footer-item">Upvotes ({review.upvotes})</div>
+          <div className="card-footer-item">
+            Comments ({review.comments_count})
+          </div>
+        </footer>
+      </div>
+    ));
+  } else {
     rendering = (
       <div title="Reload" className="has-text-centered">
-        <button disabled={errorLoading === false}  onClick={reload}>
+        <button
+          className="reload-btn"
+          disabled={errorLoading === false}
+          onClick={reload}
+        >
           <i className={"fa " + spinner + " fa-2x"} />
         </button>
       </div>
-    )
+    );
   }
 
   return (
--- src/pages/Home/TopSchools.jsx
+++ src/pages/Home/TopSchools.jsx
@@ -1,74 +1,68 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const TopSchools = ({ schools, isLoaded, errorLoading, spinner, reload }) => {
   let rendering;
-  if (isLoaded)
-  {
-    rendering = (
-      schools.map((school, index) => (
-        <div className="column is-3 has-text-centered" key={index}>
-          <div className="box top-school-box">
-            <img
-              className="box-image image is-96x96"
-              src={school.logo_url}
-              alt={school.name + " logo"}
-            />
+  if (isLoaded) {
+    rendering = schools.map((school, index) => (
+      <div className="column is-3 has-text-centered" key={index}>
+        <div className="box top-school-box">
+          <img
+            className="box-image image is-96x96"
+            src={school.logo_url}
+            alt={school.name + " logo"}
+          />
 
-            <div className="content has-text-centered">
-              <h3 className="subtitle has-text-weight-semibold is-6 ">
-                <Link to={"/school/" + school.id}>{school.name}</Link>
-              </h3>
-            </div>
+          <div className="content has-text-centered">
+            <h3 className="subtitle has-text-weight-semibold is-6 ">
+              <Link to={"/school/" + school.id}>{school.name}</Link>
+            </h3>
+          </div>
 
-            <div className="school-stat-tags">
-              <div className="bottom-pegged-display is-centered">
-                <span className="tag" title="Rank">
-                  <a className="has-text-white">{school.rank}</a>
-                  &nbsp;
-                  <i className="fa fa-trophy has-text-warning" />
-                </span>
+          <div className="school-stat-tags">
+            <div className="bottom-pegged-display is-centered">
+              <span className="tag" title="Rank">
+                <a className="has-text-white">{school.rank}</a>
+                &nbsp;
+                <i className="fa fa-trophy has-text-warning" />
+              </span>
 
-                <span className="tag" title="Rank">
-                  <a className="has-text-white">{school.rating}</a>
-                  &nbsp;
-                  <i className="fa fa-star has-text-info" />
-                </span>
+              <span className="tag" title="Rank">
+                <a className="has-text-white">{school.rating}</a>
+                &nbsp;
+                <i className="fa fa-star has-text-info" />
+              </span>
 
-                <span className="tag" title="Rank">
-                  <a className="has-text-white">
-                    {school.reviews_count}
-                  </a>
-                  &nbsp;
-                  <i className="fa fa-user has-text-success" />
-                </span>
+              <span className="tag" title="Rank">
+                <a className="has-text-white">{school.reviews_count}</a>
+                &nbsp;
+                <i className="fa fa-user has-text-success" />
+              </span>
 
-                <span className="tag" title="Rank">
-                  <a className="has-text-white">
-                    {school.reports_count}
-                  </a>
-                  &nbsp;
-                  <i className="fa fa-comment-alt has-text-danger" />
-                </span>
-              </div>
+              <span className="tag" title="Rank">
+                <a className="has-text-white">{school.reports_count}</a>
+                &nbsp;
+                <i className="fa fa-comment-alt has-text-danger" />
+              </span>
             </div>
           </div>
         </div>
-      ))
-    )
-  }
-  else
-  {
+      </div>
+    ));
+  } else {
     rendering = (
       <div className="column has-text-centered">
-      <div title="Reload" className="has-text-centered">
-        <button disabled={errorLoading === false}  onClick={reload}>
-          <i className={"fa " + spinner + " fa-2x"} />
-        </button>
+        <div title="Reload" className="has-text-centered">
+          <button
+            className="reload-btn"
+            disabled={errorLoading === false}
+            onClick={reload}
+          >
+            <i className={"fa " + spinner + " fa-2x"} />
+          </button>
+        </div>
       </div>
-      </div>
-    )
+    );
   }
 
   return (
@@ -80,9 +74,7 @@ const TopSchools = ({ schools, isLoaded, errorLoading, spinner, reload }) => {
           </h1>
           <hr />
           <br />
-          <div className="columns is-multiline">
-          {rendering}
-          </div>
+          <div className="columns is-multiline">{rendering}</div>
         </div>
       </section>
     </div>
