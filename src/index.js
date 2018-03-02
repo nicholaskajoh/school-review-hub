7c764afe115e615b2250ba49f8165b72bd1148c9
Emmanuel Awotunde <awotunde.emmanuel1@gmail.com>
2018-03-02 20:35:19 +0100

    refactor: add exact to routes with id parameters to prevent errors

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import "./assets/css/bulma.min.css";
-import "./index.css";
-import { Route, BrowserRouter, Switch } from "react-router-dom";
-import DefaultLayout from "./layouts/DefaultLayout/DefaultLayout";
-import PrivateRoute from "./auth";
-import IndexLanding from "./pages/IndexLanding/IndexLanding";
-import Home from "./pages/Home/Home";
-import SRHIndex from "./pages/SRHIndex/SRHIndex";
-import Match from "./pages/Match/Match";
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './assets/css/bulma.min.css';
+import './index.css';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import DefaultLayout from './layouts/DefaultLayout/DefaultLayout';
+import PrivateRoute from './auth';
+import IndexLanding from './pages/IndexLanding/IndexLanding';
+import Home from './pages/Home/Home';
+import SRHIndex from './pages/SRHIndex/SRHIndex';
+import Match from './pages/Match/Match';
 // import Search from './pages/Search/Search';
-import Register from "./pages/Register/Register";
-import Login from "./pages/Login/Login";
-import Logout from "./pages/Logout/Logout";
-import Profile from "./pages/Profile/Profile";
-import Help from "./pages/Help/Help";
-import RatingForm from "./pages/RatingForm/RatingForm";
-import ReviewForm from "./pages/ReviewForm/ReviewForm";
-import ReportForm from "./pages/ReportForm/ReportForm";
-import Review from "./pages/Review/Review";
-import School from "./pages/School/School";
-import Report from "./pages/Report/Report";
-import NotFound from "./pages/NotFound/NotFound";
-import registerServiceWorker from "./registerServiceWorker";
+import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
+import Logout from './pages/Logout/Logout';
+import Profile from './pages/Profile/Profile';
+import Help from './pages/Help/Help';
+import RatingForm from './pages/RatingForm/RatingForm';
+import ReviewForm from './pages/ReviewForm/ReviewForm';
+import ReportForm from './pages/ReportForm/ReportForm';
+import Review from './pages/Review/Review';
+import School from './pages/School/School';
+import Report from './pages/Report/Report';
+import NotFound from './pages/NotFound/NotFound';
+import registerServiceWorker from './registerServiceWorker';
 import TimeAgo from 'javascript-time-ago';
 // Load locale-specific relative date/time formatting rules.
 import en from 'javascript-time-ago/locale/en';
@@ -34,25 +34,25 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Switch>
-          <DefaultLayout path="/" exact component={IndexLanding} />
-          <PrivateRoute path="/home" component={Home} />
-          <DefaultLayout path="/srh-index" component={SRHIndex} />
-          <PrivateRoute path="/match" component={Match} />
-          {/* <DefaultLayout path="/search" component={Search}/> */}
-          <DefaultLayout path="/register" component={Register} />
-          <DefaultLayout path="/login" component={Login} />
-          <PrivateRoute path="/profile" component={Profile} />
-          <DefaultLayout path="/help" component={Help} />
+          <DefaultLayout path='/' exact component={IndexLanding} />
+          <PrivateRoute path='/home' component={Home} />
+          <DefaultLayout path='/srh-index' component={SRHIndex} />
+          <PrivateRoute path='/match' component={Match} />
+          {/* <DefaultLayout path='/search' component={Search}/> */}
+          <DefaultLayout path='/register' component={Register} />
+          <DefaultLayout path='/login' component={Login} />
+          <PrivateRoute path='/profile' component={Profile} />
+          <DefaultLayout path='/help' component={Help} />
           <PrivateRoute
-            path="/rate/:school1Id/:school2Id"
+            path='/rate/:school1Id/:school2Id' exact
             component={RatingForm}
           />
-          <PrivateRoute path="/add-review/:id" component={ReviewForm} />
-          <PrivateRoute path="/add-report/:id" component={ReportForm} />
-          <DefaultLayout path="/review/:id" component={Review} />
-          <DefaultLayout path="/school/:id" component={School} />
-          <DefaultLayout path="/report/:id" component={Report} />
-          <Route path="/logout" component={Logout} />
+          <PrivateRoute path='/add-review/:id' exact component={ReviewForm} />
+          <PrivateRoute path='/add-report/:id' exact component={ReportForm} />
+          <DefaultLayout path='/review/:id' exact component={Review} />
+          <DefaultLayout path='/school/:id' exact component={School} />
+          <DefaultLayout path='/report/:id' exact component={Report} />
+          <Route path='/logout' component={Logout} />
           <DefaultLayout component={NotFound} />
         </Switch>
       </BrowserRouter>
@@ -60,5 +60,5 @@ class App extends Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
