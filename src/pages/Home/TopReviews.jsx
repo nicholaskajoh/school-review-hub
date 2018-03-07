e15d0938a6eeefb9c856201aea4a7a901ef3c98e
John Ayeni <john.ayeni@stu.cu.edu.ng>
2018-03-07 11:29:16 +0100

    fix: resolve merge errors

--- src/pages/Home/TopReviews.jsx
+++ src/pages/Home/TopReviews.jsx
@@ -8,25 +8,15 @@ const TopReviews = ({ reviews, isLoaded, errorLoading, spinner, reload }) => {
     rendering = (
       <div className="columns is-multiline">
         {reviews.map(review => (
-<<<<<<< HEAD
           <div key={review.id} className="column is-4">
-=======
-        <div key={'top_review ' + review.id} className="column is-4">
-
->>>>>>> 4623390856bb37f2abe12565f1e9753a108481cb
             <div className="box">
               <article className="media">
                 <div className="media-content">
                   <div className="has-text-centered">
                     <Link
-<<<<<<< HEAD
-                      className="has-text-black-ter has-text-weight-bold"
+                      className="has-text-black-ter"
                       to={'/school/' + review.school.id}
                     >
-=======
-                      className="has-text-black-ter"
-                      to={"/school/" + review.school.id}>
->>>>>>> 4623390856bb37f2abe12565f1e9753a108481cb
                       {review.school.name}
                     </Link>
 
@@ -35,23 +25,12 @@ const TopReviews = ({ reviews, isLoaded, errorLoading, spinner, reload }) => {
 
                   <div className="content">
                     <p>
-<<<<<<< HEAD
-                      <em>
-                        {review.content.substring(0, 150).trim() +
-                          (review.content.length > 150 ? '...' : '')}"
-                      </em>
-                      <br />
-                      <br />
-=======
                       <strong>
                         "{review.content.substring(0, 150).trim() +
-                          (review.content.length > 150 ?
-                            ('...') : ('')
-                          )
-                        }"
+                          (review.content.length > 150 ? '...' : '')}"
                       </strong>
-                      <br /><br />
->>>>>>> 4623390856bb37f2abe12565f1e9753a108481cb
+                      <br />
+                      <br />
 
                       <small>
                         <em>
@@ -65,24 +44,15 @@ const TopReviews = ({ reviews, isLoaded, errorLoading, spinner, reload }) => {
                     <div className="level-left">
                       <a className="level-item has-text-dark" title="upvotes">
                         <span className="icon is-small has-text-success">
-<<<<<<< HEAD
                           <i className="fa fa-thumbs-up" />
                         </span>
-=======
-                          <i className="fa fa-thumbs-up"></i></span>
->>>>>>> 4623390856bb37f2abe12565f1e9753a108481cb
                         &nbsp;{review.upvotes}
                       </a>
                       &nbsp;&nbsp;
-<<<<<<< HEAD
-                      <a className="level-item has-text-dark">
+                      <a className="level-item has-text-dark" title="comments">
                         <span className="icon is-small has-text-warning">
-                          <i class="fas fa-comment" />
+                          <i className="fas fa-comment" />
                         </span>
-=======
-                          <a className="level-item has-text-dark" title="comments">
-                        <span className="icon is-small has-text-warning"><i className="fas fa-comment"></i></span>
->>>>>>> 4623390856bb37f2abe12565f1e9753a108481cb
                         &nbsp;{review.comments_count}
                       </a>
                     </div>
