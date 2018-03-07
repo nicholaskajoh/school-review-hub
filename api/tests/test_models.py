f7ae875fb6607c4601adb4c382dfb55051496598
Emmanuel Awotunde <awotunde.emmanuel1@gmail.com>
2018-03-07 11:14:30 +0100

    fix: fix failing tests

--- api/tests/test_models.py
+++ api/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from .factories import *
-from api.models import School, Review, Comment, Comparison, Criterion, Report, Upvote
+from api.models import Comment, Upvote
 
 class SchoolTestCase(TestCase):
     
@@ -36,7 +36,11 @@ class CommentTestCase(TestCase):
 class ComparisonTestCase(TestCase):      
     
     def setUp(self):
-        self.comparison = ComparisonFactory()
+        self.school1 = SchoolFactory()
+        self.school2 = SchoolFactory()
+        self.comparison = ComparisonFactory(school1=self.school1,
+            school2=self.school2, choice=self.school2
+        )
     
     def test_comparison_string_representation(self):
         self.assertEqual(str(self.comparison),"{0} (vs) {1} [{2}], {3}".format(
@@ -47,6 +51,15 @@ class ComparisonTestCase(TestCase):
     def test_comparison_rejects_comparing_the_same_school(self):
         # update the second school field to be the same school
         self.comparison.school2 = self.comparison.school1
+        # test if the save() method raises an execption
+        with self.assertRaises(ValueError):
+            self.comparison.save()
+
+    def test_comparison_rejects_if_choice_is_not_school1_or_school2(self):
+        # create a third school
+        school3 = SchoolFactory()
+        # update the choice to school3
+        self.comparison.choice = school3
         # test if the save() method raises an execption
         with self.assertRaises(ValueError):
             self.comparison.save()
@@ -102,5 +115,20 @@ class UpvoteTestCase(TestCase):
         if len(report.content) > 10:
             string_rep = '{} upvotes {}: \'{}...\''.format(upvote.upvoter.username,
                 Upvote.REPORT, report.content[:10]
+            )
+        self.assertEqual(str(upvote), string_rep)
+
+    def test_upvote_string_representation_for_comment(self):
+        review = ReviewFactory()
+        comment = CommentFactory(entity_id=review.pk)
+        upvote = UpvoteFactory(entity_id=comment.pk, 
+            entity=Upvote.COMMENT
+        )
+        string_rep = '{} upvotes {}: \'{}\''.format(upvote.upvoter.username,
+            Upvote.COMMENT, comment.comment
+        )
+        if len(comment.comment) > 10:
+            string_rep = '{} upvotes {}: \'{}...\''.format(upvote.upvoter.username,
+                Upvote.COMMENT, comment.comment[:10]
             )
         self.assertEqual(str(upvote), string_rep)
--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -3,7 +3,9 @@ from .factories import *
 from django.shortcuts import reverse
 from api.serializers import *
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-# from api.models import School, Review, Comment, Comparison, Criterion, Report, Upvote
+from api.management.commands import rate_and_rank
+# from unittest import mock
+import datetime
 
 def paginate(input_list, page, results_per_page=10):
     paginator = Paginator(input_list, results_per_page)
@@ -22,8 +24,8 @@ class SchoolReviewsViewTestCase(TestCase):
 
     def setUp(self):
         self.school = SchoolFactory()
-        # create 11 reviews for pagination
-        for x in range(11):
+        # create many reviews for pagination
+        for x in range(21):
             ReviewFactory(school=self.school)
 
     def test_if_school_reviews_endpoint_returns_review(self):
@@ -51,7 +53,7 @@ class SchoolReviewsViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         reviews = Review.objects.filter(school=self.school).order_by('-updated_at')
         # get result of the last page
-        reviews = paginate(reviews, 2, 10)
+        reviews = paginate(reviews, 3, 10)
         serialized = ReviewSerializer(reviews, many=True)
         # it should return the last page results
         self.assertEqual(response.data, serialized.data)
@@ -66,6 +68,12 @@ class SchoolReviewsViewTestCase(TestCase):
 
         url = reverse('school_reviews', kwargs={'id':self.school.pk,'page':2})
         response = self.client.get(url)
+        # any middle page should have 'X-Has-Previous' and 'X-Has-Next'
+        self.assertEqual(response['X-Has-Previous'], 'True')
+        self.assertEqual(response['X-Has-Next'], 'True')
+
+        url = reverse('school_reviews', kwargs={'id':self.school.pk,'page':3})
+        response = self.client.get(url)
         # the last page should have 'X-Has-Previous' not 'X-Has-Next'
         self.assertEqual(response['X-Has-Previous'], 'True')
         self.assertEqual(response['X-Has-Next'], 'False')
@@ -74,8 +82,8 @@ class SchoolReportsViewTestCase(TestCase):
 
     def setUp(self):
         self.school = SchoolFactory()
-        # create 11 reports for pagination
-        for x in range(11):
+        # create many reports for pagination
+        for x in range(21):
             ReportFactory(school=self.school)
 
     def test_if_school_reports_endpoint_returns_report(self):
@@ -103,7 +111,7 @@ class SchoolReportsViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         reports = Report.objects.filter(school=self.school).order_by('-updated_at')
         # get result of the last page
-        reports = paginate(reports, 2, 10)
+        reports = paginate(reports, 3, 10)
         serialized = ReportSerializer(reports, many=True)
         # it should return the last page results
         self.assertEqual(response.data, serialized.data)
@@ -119,6 +127,12 @@ class SchoolReportsViewTestCase(TestCase):
 
         url = reverse('school_reports', kwargs={'id':self.school.pk,'page':2})
         response = self.client.get(url)
+        # any middle page should have 'X-Has-Previous' and 'X-Has-Next'
+        self.assertEqual(response['X-Has-Previous'], 'True')
+        self.assertEqual(response['X-Has-Next'], 'True')
+
+        url = reverse('school_reports', kwargs={'id':self.school.pk,'page':3})
+        response = self.client.get(url)
         # the last page should have 'X-Has-Previous' not 'X-Has-Next'
         self.assertEqual(response['X-Has-Previous'], 'True')
         self.assertEqual(response['X-Has-Next'], 'False')
@@ -127,10 +141,7 @@ class SchoolReportsViewTestCase(TestCase):
 class SchoolViewTestCase(TestCase):
 
     def setUp(self):
-        self.school = SchoolFactory(
-          name='CU',location='Ogun',logo_url='',website='covenantuniversity.edu.ng',
-          rank=1,rating=140.0,
-        )
+        self.school = SchoolFactory()
 
     def test_if_school_endpoint_returns_school(self):
         url = reverse('school', kwargs={'id':self.school.pk})
@@ -149,18 +160,9 @@ class SchoolViewTestCase(TestCase):
 class SchoolsListViewTestCase(TestCase):
 
     def setUp(self):
-        self.school = SchoolFactory(
-          name='CU',location='Ogun',logo_url='',website='covenantuniversity.edu.ng',
-          rank=1,rating=140.0,
-        )
-        self.school2 = SchoolFactory(
-          name='ABUAD',location='Ekiti',logo_url='',website='abuad.edu.ng',
-          rank=2,rating=130.0,
-        )
-        self.school3 = SchoolFactory(
-          name='LMU',location='Kwara',logo_url='',website='lmu.edu.ng',
-          rank=3,rating=120.0,
-        )
+        # create five schools
+        for x in range(5):
+            SchoolFactory()
 
     def test_if_school_list_endpoint_returns_all_schools(self):
         url = reverse('schools_list')
@@ -238,29 +240,16 @@ class TopSchoolsViewTestCase(TestCase):
 class SRHIndexViewTestCase(TestCase):
 
     def setUp(self):
-        self.school = SchoolFactory(
-        name='CU',location='Ogun',logo_url='',website='covenantuniversity.edu.ng',
-        rank=1,rating=140.0,
-        )
-        self.school2 = SchoolFactory(
-        name='ABUAD',location='Ekiti',logo_url='',website='abuad.edu.ng',
-        rank=2,rating=130.0,
-        )
-        self.school3 = SchoolFactory(
-        name='LMU',location='Kwara',logo_url='',website='lmu.edu.ng',
-        rank=3,rating=120.0,
-        )
-        self.school4 = SchoolFactory(
-        name='Harvard',location='Abia',logo_url='',website='havard.edu.ng',
-        rank=3,rating=100.0,
-        )
-        self.school5 = SchoolFactory(
-        name='Crest',location='Kaduna',logo_url='',website='crest.edu.ng',
-        rank=5,rating=80.0,
-        )
-        # make 20 more schools
-        for x in range(20):
-            SchoolFactory()
+        # create schools and comparisons
+        for x in range(30):
+            school1 = SchoolFactory()
+            school2 = SchoolFactory()
+            ComparisonFactory(school1=school1, school2=school2, choice=school2)
+        # run the rate_and_rank command
+        # so there would be rating and ranking for the schools
+        cmd = rate_and_rank.Command()
+        cmd.handle()
+
 
     def test_if_srh_index_ranks_schools_correctly(self):
         url = reverse('srh_index', kwargs={'page':1})
@@ -279,7 +268,7 @@ class SRHIndexViewTestCase(TestCase):
         serialized = SchoolSerializer(schools, many=True)
         self.assertEqual(response.data, serialized.data)
 
-    def test_if_srh_index_returns_remaining_5_schools_on_page_two(self):
+    def test_if_srh_index_returns_next_20_schools_on_page_two(self):
         url = reverse('srh_index', kwargs={'page':2})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
@@ -295,7 +284,7 @@ class SRHIndexViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         schools = School.objects.all().order_by('rank')
         # get result of the last page
-        schools = paginate(schools, 2, 20)
+        schools = paginate(schools, 3, 20)
         serialized = SchoolSerializer(schools, many=True)
         # it should return the last page results
         self.assertEqual(response.data, serialized.data)
@@ -310,6 +299,187 @@ class SRHIndexViewTestCase(TestCase):
 
         url = reverse('srh_index', kwargs={'page':2})
         response = self.client.get(url)
+        # any middle page should have 'X-Has-Previous' and 'X-Has-Next'
+        self.assertEqual(response['X-Has-Previous'], 'True')
+        self.assertEqual(response['X-Has-Next'], 'True')
+
+        url = reverse('srh_index', kwargs={'page':3})
+        response = self.client.get(url)
         # the last page should have 'X-Has-Previous' not 'X-Has-Next'
         self.assertEqual(response['X-Has-Previous'], 'True')
         self.assertEqual(response['X-Has-Next'], 'False')
+
+
+class ReviewViewTestCase(TestCase):
+
+    def setUp(self):
+        self.review = ReviewFactory()
+
+    def test_if_review_endpoint_returns_review(self):
+        url = reverse('review', kwargs={'id':self.review.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        serialized = ReviewSerializer(self.review)
+        self.assertEqual(response.data, serialized.data)
+
+    def test_if_review_endpoint_returns_error_on_wrong_review_id(self):
+        # put an id of a review that does not exist
+        url = reverse('review', kwargs={'id':5})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+
+class ReportViewTestCase(TestCase):
+
+    def setUp(self):
+        self.report = ReportFactory()
+
+    def test_if_report_endpoint_returns_report(self):
+        url = reverse('report', kwargs={'id':self.report.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        serialized = ReportSerializer(self.report)
+        self.assertEqual(response.data, serialized.data)
+
+    def test_if_report_endpoint_returns_error_on_wrong_report_id(self):
+        # put an id of a report that does not exist
+        url = reverse('report', kwargs={'id':5})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+
+class CommentsViewTestCase(TestCase):
+
+    def setUp(self):
+        self.review = ReviewFactory()
+        for x in range(11):
+            CommentFactory(entity='review', entity_id=self.review.pk)
+
+
+    def test_if_comments_endpoint_returns_comments(self):
+        url = reverse('comments', kwargs={'entity': 'review',
+            'id':self.review.pk, 'page': 1})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        comments = Comment.objects.filter(entity='review', entity_id=self.review.pk)\
+            .order_by('-created_at')
+        
+        comments = paginate(comments, 1, 5)
+
+        serialized = CommentSerializer(comments, many=True)
+        self.assertEqual(response.data, serialized.data)
+
+    def test_if_comments_endpoint_has_has_previous_and_has_next_keys(self):
+        url = reverse('comments', kwargs={'entity': 'review',
+            'id':self.review.pk, 'page': 1})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        # page one should have 'X-Has-Next' but 'X-Has-Previous'
+        self.assertEqual(response['X-Has-Next'], 'True')
+        self.assertEqual(response['X-Has-Previous'], 'False')
+
+        url = reverse('comments', kwargs={'entity': 'review',
+            'id':self.review.pk, 'page': 2})
+        response = self.client.get(url)
+        # any middle page should have 'X-Has-Previous' and 'X-Has-Next'
+        self.assertEqual(response['X-Has-Previous'], 'True')
+        self.assertEqual(response['X-Has-Next'], 'True')
+
+        url = reverse('comments', kwargs={'entity': 'review',
+            'id':self.review.pk, 'page': 3})
+        response = self.client.get(url)
+        # the last page should have 'X-Has-Previous' not 'X-Has-Next'
+        self.assertEqual(response['X-Has-Previous'], 'True')
+        self.assertEqual(response['X-Has-Next'], 'False')
+
+
+# mock the date created.
+class TopReviewsViewTestCase(TestCase):
+
+    def setUp(self):
+        # create reviews whose date is 3 months before timezone.now()
+        for x in range(5):
+            review = ReviewFactory()
+            review.created_at = datetime.datetime(2018, 1, 6, 20, 32, 35, 74424)
+            review.save()
+        
+        # create reviews whose date is ealier than 3 months
+        for x in range(5):
+            review = ReviewFactory()
+            review.created_at = datetime.datetime(2016, 12, 6, 20, 32, 35, 74424)
+            review.save()
+    
+    # @mock.patch('api.views.datetime.timedelta')
+    # @mock.patch('api.views.timezone.now')
+    def test_if_top_reviews_endpoint_returns_top_5_reviews(self):
+    # def test_if_top_review_endpoint_returns_top_5_reviews(self, mock_now, mock_timedelta):        
+        # def func(days):
+        #     return datetime.datetime(days)            
+        # mock_timedelta.return_value = func
+
+        # def func2():
+        #     return datetime.datetime(2018, 3, 6, 20, 35, 25, 650482)
+        # mock_now.return_value = func2
+
+        url = reverse('top_reviews')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        # mock_timedelta.assert_called_with(30 * 3)
+        # mock_now.assert_called()
+
+        # assert that there are 10 reviews in the database
+        self.assertEqual(Review.objects.count(), 10)
+
+        reviews = Review.objects.filter(
+            created_at__gt=datetime.datetime(2017, 12, 6, 20, 35, 25, 650482)
+        ).order_by('-created_at')
+        # assert that there are 5 reviews within specified date
+        self.assertEqual(reviews.count(), 5)
+        serialized = ReviewSerializer(reviews, many=True)
+        self.assertEqual(response.data, serialized.data)
+
+
+class TopReportsViewTestCase(TestCase):
+
+    def setUp(self):
+        # create reports whose date is 3 months before timezone.now()
+        for x in range(5):
+            report = ReportFactory()
+            report.created_at = datetime.datetime(2018, 1, 6, 20, 32, 35, 74424)
+            report.save()
+        
+        # create reports whose date is ealier than 3 months
+        for x in range(5):
+            report = ReportFactory()
+            report.created_at = datetime.datetime(2016, 12, 6, 20, 32, 35, 74424)
+            report.save()
+    
+    # @mock.patch('api.ports.datetime.timedelta')
+    # @mock.patch('api.ports.timezone.now')
+    def test_if_top_reports_endpoint_returns_top_5_reports(self):
+    # def test_if_top_report_endpoint_returns_top_5_reports(self, mock_now, mock_timedelta):        
+        # def func(days):
+        #     return datetime.datetime(days)            
+        # mock_timedelta.return_value = func
+
+        # def func2():
+        #     return datetime.datetime(2018, 3, 6, 20, 35, 25, 650482)
+        # mock_now.return_value = func2
+
+        url = reverse('top_reports')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        # mock_timedelta.assert_called_with(30 * 3)
+        # mock_now.assert_called()
+
+        # assert that there are 10 reports in the database
+        self.assertEqual(Report.objects.count(), 10)
+
+        reports = Report.objects.filter(
+            created_at__gt=datetime.datetime(2017, 12, 6, 20, 35, 25, 650482)
+        ).order_by('-created_at')
+        # assert that there are 5 reports within specified date
+        self.assertEqual(reports.count(), 5)
+        serialized = ReportSerializer(reports, many=True)
+        self.assertEqual(response.data, serialized.data)
